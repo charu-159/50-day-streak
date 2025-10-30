9faba6359ed572ef5e5cc2061ed965716b021e21
charu-159 <charupatel159@gmail.com>
2025-10-30 15:39:24 +0530

    first

--- day17th.c
+++ day17th.c
@@ -1,2 +1,26 @@
 //find intersection of two sortedarray
+#include <stdio.h>
+
+int main() {
+    int arr1[] = {1, 2, 3, 4, 5};
+    int arr2[] = {2, 3, 5, 7, 8};
+    int n1 = sizeof(arr1)/sizeof(arr1[0]);
+    int n2 = sizeof(arr2)/sizeof(arr2[0]);
+    int i = 0, j = 0;
+
+    printf("Intersection: ");
+    while (i < n1 && j < n2) {
+        if (arr1[i] < arr2[j])
+            i++;
+        else if (arr1[i] > arr2[j])
+            j++;
+        else {
+            printf("%d ", arr1[i]);
+            i++;
+            j++;
+        }
+    }
+    printf("\n");
+    return 0;
+}
 
