253103f55dbb3f0fe2bc30e13e133ac9e9eb3170
charu-159 <charupatel159@gmail.com>
2025-11-01 14:14:15 +0530

    .

--- day11th.c
+++ day11th.c
@@ -6,5 +6,43 @@ int main(){
     scanf("%d",&n);
 
     printf("enter the element of array:");
-    
+
+     int a[n],i=0;
+     while(i<n)
+  {
+    scanf("%d",&a[i]);
+    i++;
+  }
+  int pos[n],neg[n];
+  int p=0,q=0;
+  //sepparate +ve and -ve elements
+  for(i=0;i<n;i++)
+  {
+    if(a[i]>=0)
+    pos[p++]=a[i];
+    else 
+    neg[q++]=a[i];
+  }
+  //merge 2 array alternatively
+  i=0;
+  int j=0,k=0;
+  while(i<p&&j<q)
+  {
+    a[k++]=pos[i++];
+    a[k++]=neg[j++];
+  }
+  //fill remaining +ve
+  while(i<p)
+  a[k++]=pos[i++];
+  //fill remaining -ve
+  while(j<q)
+  a[k++]=neg[j++];
+  printf("rearranged array: { ");
+  for(int m=0;m<n;m++)
+  {
+    printf("%d",a[m]);
+    if(m<n-1)
+    printf(", ");
+  }
+  printf("}");
 }
---
+++ string/day25.c
@@ -0,0 +1,1 @@
+//check if two string are anagrams
